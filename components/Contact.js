422f26c9ba327b2669c4d58d668ebb5a5494cf26
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-10-08 19:25:20 +0200

    Add JSX code

---
+++ components/Contact.js
@@ -0,0 +1,24 @@
+"use strict"
+
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+    
+    render: function() {
+        return (
+            <div className={'contactItem'}>
+                <img className={'contactImage'} src={'./Contacts_Silver.png'}/>
+                <p className={'contactLabel'}>
+                    Imię: {this.props.contact.firstName}
+                </p>
+                <p className={'contactLabel'}>
+                    Nazwisko: {this.props.contact.lastName}
+                </p>
+                <a href={'mailto:' + this.props.item.email}>
+                    {this.props.item.email}
+                </a>
+            </div>
+        )
+    }
+});
---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict"
+
+var app = React.createElement(App);
+
+// przypięcie komponentu App do drzewa DOM
+ReactDOM.render(App, document.getElementById('app'));
