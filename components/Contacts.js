8374df10ed6c880031b9f1d26f7f4f68a0c94d51
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-10-09 18:41:48 +0200

    Correct mistake

--- components/Contacts.js
+++ components/Contacts.js
@@ -4,18 +4,14 @@ var Contacts = React.createClass({
     propTypes: {
         items: React.PropTypes.array.isRequired,
     },
-  
+      
     render: function() {
         var contacts = this.props.items.map(function(contact) {
-
-        return 
-
-            <Contact item={contact} key={contact.id}/>;
-
+            return <Contact item={contact} key={contact.id}/>;
         });
-  
+      
         return (
-            <ul className={'contactsList'}>{contacts}</ul>
+            <ul className={'contactsList'}> {contacts} </ul>
         );
     }
-});
+})
